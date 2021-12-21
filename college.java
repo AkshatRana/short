bcdc68caf657e7a0b271ca944ec2caed05df2d92
AkshatRana <akshat.xworkz@gmail.com>
2021-12-21 08:01:30 +0530

    college files are added

---
+++ college.java
@@ -0,0 +1,17 @@
+	class College{
+	public static void main(String[] args){
+	byte []roll= {1,2,3,4,5,6,7,8,9,10,11};
+	System.out.println(roll[0]);
+	System.out.println(roll[1]);
+	System.out.println(roll[2]);
+	System.out.println(roll[3]);
+	System.out.println(roll[4]);
+	System.out.println(roll[5]);
+	System.out.println(roll[6]);
+	System.out.println(roll[7]);
+	System.out.println(roll[8]);
+	System.out.println(roll[9]);
+	System.out.println(roll[10]);
+
+	}
+}
